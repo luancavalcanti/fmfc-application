8c008c7dd32a4d92259b5002091ad9dfebafb7f8
luancavalcanti <luanscavalcanti@gmail.com>
2025-01-14 13:04:26 -0500

    Correção de algumas nomenclaturas de imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { UserProvider } from './contex/userContex.jsx'
-import { EmployeeProvider } from './contex/EmployeeContext.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
-    <EmployeeProvider>
       <App />
-    </EmployeeProvider>
     </UserProvider>
   </StrictMode>,
 )
